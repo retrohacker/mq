faa5437e570e4406d4ccca7c01aba535e760ce56
wblankenship <william.jblankenship@gmail.com>
2015-09-07 11:22:31 -0400

    Properly receive objects with handlers

--- examples/get.js
+++ examples/get.js
@@ -5,16 +5,24 @@ mq.connect(function(e) {
     console.error(JSON.stringify(e))
     return process.exit(1)
   }
-  console.log("Service started...")
-  mq.trigger('get:user(id)',1,function(e,user) {
-    console.log("WOAH! I GOT A RESPONSE!")
-    if(e) console.log(JSON.stringify(e))
-    if(user) console.log(user)
-  })
-  mq.handle('get:user(id)',function(e,id,cb) {
+  mq.handle('get:user',function(e,id,cb) {
     console.log("HELL YES, I CAN HANDLE THIS!")
-    if(e) console.log(JSON.stringify(e))
-    if(id) console.log(id)
+    if(e) console.log("handle:",typeof e,"e:",e)
+    if(id) console.log("handle:",typeof id,"id:",id)
     cb({uid:1,fname:"William",lname:"Blankenship",email:"william.jblankenship@gmail.com"})
   })
+
+  console.log("Service started...")
+
+  trigger("get:user",{uid:1,email:"william.jblankenship@gmail.com"})
+
 })
+
+function trigger(key,value) {
+  mq.trigger(key,value,function(e,user) {
+    console.log("WOAH! I GOT A RESPONSE!")
+    if(e) console.log("trigger:",typeof e,"e:",e)
+    if(user) console.log("trigger:",typeof user,"user:",user)
+    trigger(key,e)
+  })
+}
